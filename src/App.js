2d5ae9647365b8a53aa36cedbdde0a91beaffb96
Kenzo VONGKINGKEO <freeken69@gmail.com>
2023-10-27 11:12:04 +0200

    design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Navbar from "./components/Navbar";
+import Competences from "./pages/Competences";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+        <body>
+        <Competences></Competences>
+        </body>
     </div>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,36 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faCogs, faCode, faEnvelope, faBars} from "@fortawesome/free-solid-svg-icons";
-import "../App.css"
+import { faHome, faCogs, faCode, faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
+import "../App.css";
 
 export default function Navbar() {
     return (
-        <div className="wrapper">
-            <div className="menu-btn"> <FontAwesomeIcon icon={faBars}></FontAwesomeIcon></div>
-                <ul className="menu">
-                    <li>
-                        <FontAwesomeIcon icon={faHome} />
-                        <a href="#"></a>Accueil
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faCogs} />
-                        <a href="#"></a>Compétences
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faCode} />
-                        <a href="#"></a>Projets
-                    </li>
-                    <li>
-                        <FontAwesomeIcon icon={faEnvelope} />
-                        <a href="#"></a>Contact
-                    </li>
-                </ul>
+        <div>
+            <div className="container mx-auto p-4">
+                <div className="flex items-center justify-center">
+                    {/*<div className="menu-btn text-white">*/}
+                    {/*    <FontAwesomeIcon icon={faBars} />*/}
+                    {/*</div>*/}
+                    <ul className="menu flex items-center space-x-4">
+                        <li className="text-white hover:bg-white hover:text-black rounded-lg p-2 transition-all duration-300">
+                            <FontAwesomeIcon icon={faHome} />
+                            <a href="#">Accueil</a>
+                        </li>
+                        <li className="text-white hover:bg-white hover:text-black rounded-lg p-2 transition-all duration-300">
+                            <FontAwesomeIcon icon={faCogs} />
+                            <a href="#">Compétences</a>
+                        </li>
+                        <li className="text-white hover:bg-white hover:text-black rounded-lg p-2 transition-all duration-300">
+                            <FontAwesomeIcon icon={faCode} />
+                            <a href="#">Projets</a>
+                        </li>
+                        <li className="text-white hover:bg-white hover:text-black rounded-lg p-2 transition-all duration-300">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
